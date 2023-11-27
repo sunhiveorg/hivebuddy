6e3a9bb74433e932d77cbbaf8fa4219b8972c348
anna.deineko <anna.deineko@student.kdg.be>
2023-11-27 12:20:43 +0100

    nav bar

---
+++ src/main/resources/templates/hive-buddy-ui/src/components/Navbar/consts/navBarItems.js
@@ -0,0 +1,32 @@
+export const mainNavBarIcons = [
+    {
+        id: 0,
+        icon: '',
+        label: 'smth',
+        route: 'route',
+    },
+    {
+        id: 1,
+        icon: '',
+        label: 'smth',
+        route: 'route',
+    },
+    {
+        id: 2,
+        icon: '',
+        label: 'smth',
+        route: 'route',
+    },
+    {
+        id: 3,
+        icon: '',
+        label: 'smth',
+        route: 'route',
+    },
+    {
+        id: 4,
+        icon: '',
+        label: 'smth',
+        route: 'route',
+    }
+]
