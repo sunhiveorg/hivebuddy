e6193f9fcf3a0d901e6eaf1afd85d40462515ec5
Rossy <git.umbrella830@slmails.com>
2023-12-04 14:53:33 +0100

    moved website to different repo

--- src/main/resources/templates/hive-buddy-ui/src/pages/Strengths/Strengths.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Strengths = () => {
-    return (
-        <div>Strengths</div>
-    )
-}
-export default Strengths
--- src/main/resources/templates/hive-buddy-ui/src/services/SensorDataService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const SENSORS_API_BASE_URL = "http://localhost:8080/api/v1/data";
-
-class SensorDataService {
-
-}
-export default SensorDataService
